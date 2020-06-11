5cd452d065734ad1f63dfa17b86f42e439b3b561
RayWang <2271272653@qq.com>
2020-06-11 12:40:24 +0800

    update

--- Acme.BookStore/aspnet-core/src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
+++ Acme.BookStore/aspnet-core/src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -7,6 +7,9 @@ namespace Acme.BookStore.Web
         public BookStoreWebAutoMapperProfile()
         {
             //Define your AutoMapper configuration here for the Web project.
+
+            //添加dto到新增编辑dto的映射
+            CreateMap<BookDto, CreateUpdateBookDto>();
         }
     }
 }
--- Acme.BookStore/aspnet-core/src/Acme.BookStore.Web/Pages/Book/index.js
+++ Acme.BookStore/aspnet-core/src/Acme.BookStore.Web/Pages/Book/index.js
@@ -1,7 +1,33 @@
 ﻿$(function () {
+    var l = abp.localization.getResource('BookStore');
+
+    var createModal = new abp.ModalManager(abp.appPath + 'Book/CreateModal');
+    var editModal = new abp.ModalManager(abp.appPath + 'Book/EditModal');
+
+    //分页列表：
     var dataTable = $('#BookTable').DataTable(abp.libs.datatables.normalizeConfiguration({
+        processing: true,
+        serverSide: true,
+        paging: true,
+        searching: false,
+        autoWidth: false,
+        scrollCollapse: true,
+        order: [[1, "asc"]],
         ajax: abp.libs.datatables.createAjax(acme.bookStore.book.getList),
         columnDefs: [
+            {
+                rowAction: {
+                    items:
+                        [
+                            {
+                                text: l('Edit'),
+                                action: function (data) {
+                                    editModal.open({ id: data.record.id });
+                                }
+                            }
+                        ]
+                }
+            },
             { data: "name" },
             { data: "type" },
             { data: "publishDate" },
@@ -9,4 +35,19 @@
             { data: "creationTime" }
         ]
     }));
+
+
+    //新建：
+    createModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+    $('#NewBookButton').click(function (e) {
+        e.preventDefault();
+        createModal.open();
+    });
+
+    //编辑：
+    editModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
 });
